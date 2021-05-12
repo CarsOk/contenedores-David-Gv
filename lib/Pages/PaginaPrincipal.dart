0a67112f1b1f51e9162da10223aeba3a301c0966
David Garcia <davidjosegarcia12@gmail.com>
2021-05-12 01:00:06 -0500

    Se realizo un menu en la parte superior de la interfaz grafica

---
+++ lib/Pages/PaginaPrincipal.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class PaginaPrincipal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(
+              child: Text('Starblock',
+                  style:
+                      TextStyle(fontSize: 25.0, fontWeight: FontWeight.w400))),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                  accountName: Text('David Garcia'),
+                  accountEmail: Text('Davidgarcia@Starblock.co'),
+                  currentAccountPicture: CircleAvatar(
+                      backgroundColor: Colors.amber,
+                      child: Text(
+                        'D',
+                        style: TextStyle(fontSize: 30.0),
+                      ))),
+              ListTile(
+                  title: Text('Opciones'),
+                  leading: Icon(Icons.settings),
+                  onTap: () {}),
+              ListTile(
+                  title: Text('Contactos'),
+                  leading: Icon(Icons.contacts),
+                  onTap: () {}),
+            ],
+          ),
+        ),
+        body: Center(
+            child: Column(
+          children: [
+            Text('Version 1.0.0 BETA',
+                style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 14.0,
+                    color: Colors.black)),
+            Text('Esta aplicacion se encuentra en desarrollo beta',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 20.0,
+                ),
+                textAlign: TextAlign.center),
+            Icon(Icons.add_alert, size: 45.0, color: Colors.red),
+          ],
+        )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,16 @@
 import 'package:flutter/material.dart';
+import 'Pages/PaginaPrincipal.dart';
 
-void main() {
-  runApp(StarBlock());
-}
+void main() => runApp(StarBlock());
 
 class StarBlock extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Mi aplicacion movil',
-      theme: ThemeData(primarySwatch: Colors.yellow),
+      theme: ThemeData(primarySwatch: Colors.cyan),
       debugShowCheckedModeBanner: false,
       home: PaginaPrincipal(),
     );
-  }
-}
-
-class PaginaPrincipal extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Center(
-              child: Text('Starblock',
-                  style:
-                      TextStyle(fontSize: 25.0, fontWeight: FontWeight.w400))),
-        ),
-        body: Center(
-            child: Column(
-          children: [
-            Text('Version 1.0.0 BETA',
-                style: TextStyle(
-                    fontStyle: FontStyle.italic,
-                    fontSize: 14.0,
-                    color: Colors.black)),
-            Text('Esta aplicacion se encuentra en desarrollo beta',
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 20.0,
-                ),
-                textAlign: TextAlign.center),
-            Icon(Icons.add_alert, size: 45.0, color: Colors.red)
-          ],
-        )));
   }
 }
